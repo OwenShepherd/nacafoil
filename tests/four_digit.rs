7a6977ec625d87a245d6636101a03a2ef5b994fe
Owen Shepherd <owsh8290@colorado.edu>
2023-06-05 10:32:09 -0600

    Added more test data.
    Need to figure out how to use serde.

--- tests/four_digit.rs
+++ tests/four_digit.rs
@@ -1,3 +1,10 @@
-use serde::{Serialize, Deserialize};
+use serde_json::{Serialize, Deserialize};
+use nacafoil;
 
+impl Deserialize for nacafoil::Airfoil {
+    fn deserialize<S>(&self, deserializer: S) -> Result<S::Ok, S::Error>
+    where {
+        S
+    }
+}
 
