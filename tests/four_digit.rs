eddf449bc8cd609f3a0a8dc9930b6394fd17fc63
Owen Shepherd <owsh8290@colorado.edu>
2023-06-05 19:17:56 -0600

    refactoring

--- tests/four_digit.rs
+++ tests/four_digit.rs
@@ -21,19 +21,88 @@ fn read_data() -> Result<Vec<JSONFoil>, Box<dyn Error>> {
     Ok(foils)
 }
 
+fn find_closest(coordinates: &Vec<f64>, ordinates: &Vec<f64>, desired: f64) -> (f64, f64) {
+    let num = coordinates.len();
+    let mut current_min = std::f64::MAX;
+    let mut target_x = 0.0;
+    let mut target_y = 0.0;
+    for index in 0..num {
+        let test = coordinates[index];
+        let current_diff = (test - desired).abs();
+        if current_diff < current_min {
+            current_min = current_diff;
+            target_x = test;
+            target_y = ordinates[index];
+        }
+    }
+    (target_x, target_y)
+}
+
+fn assert_within_percent_chord(foil: &JSONFoil, airfoil: &nacafoil::Airfoil, percent: f64, upper: bool) -> Option<(f64, f64, String)> {
+    let mut exp;
+    let predicted_x;
+    let predicted_y;
+    let mut notch = 1.0;
+    if upper {
+        exp = &foil.upper;
+        predicted_x = &airfoil.surface.upper_x;
+        predicted_y = &airfoil.surface.upper_y;
+    } else {
+        exp = &foil.lower;
+        if exp==&[(-2.0 as f64, -2.0 as f64)].to_vec() {
+            exp = &foil.upper;
+            notch = -1.0;
+        }
+        predicted_x = &airfoil.surface.lower_x;
+        predicted_y = &airfoil.surface.lower_y;
+    }
+    let num = exp.len();
+    for index in 0..num {
+        let current_test_x = exp[index].0;
+        let current_test_y = exp[index].1 * notch;
+        let predicted = find_closest(&predicted_x, &predicted_y, current_test_x);
+        if (predicted.0 - current_test_x).abs() > percent / 100.0 {
+            return Some((predicted.0, current_test_x, "x".to_string()));
+        } else if (predicted.1 - current_test_y).abs() > percent / 100.0 {
+            return Some((predicted.1, current_test_y, "y".to_string()));
+        }
+    }
+    None
+}
+
 #[test]
 fn compare_surface_with_naca_technical_824() {
     let foils = read_data().unwrap();
     for foil in foils {
         if foil.name.len() == 4 {
-            let m = (foil.name.as_bytes()[0] as char).to_digit(2).unwrap() as f64 / 100.0;
-            let p = (foil.name.as_bytes()[1] as char).to_digit(2).unwrap() as f64 / 10.0;
-            let t = &foil.name[2..3].parse::<f64>().unwrap() / 100.0;
             let c: f64 = 1.0;
-            let possible_n: [i32; 4] = [100, 1000, 10000, 100000];
-            let four_digit_diff = 0.002 * c;
+            let possible_n: [i32; 5] = [100, 1000, 10000, 100000, 1000000];
+            let mut upper_result;
+            let mut lower_result;
+            let mut assert_message: String = "".to_string();
+            let mut airfoil;
+            let mut found_value = false;
             for num in possible_n {
-                let airfoil_results = nacafoil::generate_airfoil_boundary(m, p, t, c, num);
+                airfoil = nacafoil::Airfoil::new(foil.name.clone(), c, num);
+                upper_result = assert_within_percent_chord(&foil, &airfoil, 0.2, true);
+                lower_result = assert_within_percent_chord(&foil, &airfoil, 0.2, false);
+                match (upper_result, lower_result) {
+                    (Some(rtr), _) | (_, Some(rtr))=> {
+                        assert_message = format!("Cannot generate NACA {} within {} percent of chord.\nFailed on predicted_{}: {}, expected_{}: {}", foil.name, "0.2", rtr.2, rtr.0, rtr.2, rtr.1);
+                        continue;
+                    }
+                    (None, None) => {
+                        assert!(true);
+                        found_value = true;
+                        println!("Generates NACA {} within {} percent of chord with N = {}", foil.name, "0.2", num);
+                        break;
+                    }
+                }
+            }
+            if found_value {
+                continue;
+            } else {
+                assert!(false, "{}", assert_message);
             }
         }
     }
